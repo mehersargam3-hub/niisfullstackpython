f999d58a7000e600fd8ca31bba28d302088bc955
meherasargam <mehersargam3@gmail.com>
2026-02-19 11:55:29 +0530

    full stack python program

---
+++ pythonniis/program1.py
@@ -0,0 +1,8 @@
+#swapping using 3rd variable
+a=10
+b=20
+print("before swapping a=",a,"b=",b)
+t=a//b
+a=b//b
+b=t//b
+print("after swapping a=",a,"b=",b)
---
+++ pythonniis/program2.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+c=30
+print(a,b,c)
+t=c 
+c=b
+b=a 
+a=t 
+print(a,b,c)
+a,b,c=c,b,a 
